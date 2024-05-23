9b8173c95876c08a6e1ea96d2ad531f046d5ee79
Kenny <kekeasouza@gmail.com>
2024-05-22 21:41:44 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Teste do GIT")
+
+melhor_titulo = "Libertadores 2020"
+
+melhor_jogo = "Palmeiras x Gremio 2014"
+
+melhor_jogador = "Dudu Guerreiro"
+
+melhor_camisa = "Home 2018"
+
+print(melhor_camisa)
+
+print(melhor_jogador)
+
+print(melhor_titulo)
+
+print(melhor_jogo)
